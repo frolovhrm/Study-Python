2f6dca5d62a1e8b551438c60e8e63f56b96fb607
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-05-09 22:48:35 +0300

    is it all

---
+++ Глава 7/part7_1.py
@@ -0,0 +1,23 @@
+# Упражнение 1
+
+message = input('What car do you want to get: ')
+print(f'Let me see if I can find you a {message}')
+
+# Упражнение 2
+
+quest = input('How many person, will be stay: ')
+quest = int(quest)
+
+if quest > 8:
+    print('Sorry You have to wait')
+else:
+    print('Your table is ready')
+
+# Упражнение 3
+
+num = input('Input your number: ')
+
+if int(num) % 10 == 0:
+    print('Число кратное 10!')
+else:
+    print('Число не кратное 10')
