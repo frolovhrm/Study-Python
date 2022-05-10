4b988f635a0d59cc5709c93fbaa7cc583500b737
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-05-11 00:20:00 +0300

    is it all

---
+++ Глава 7/part7_4_7.py
@@ -0,0 +1,21 @@
+# Упражнение 4
+
+# while True:
+#     topping = input("what topping would you want to add: ")
+#     if topping == 'quit':
+#         break
+#     else:
+#         print('topping is added')
+
+# Упражнение 5
+
+active = True
+while active:
+    age = input('how old are you? : ')
+    age = int(age)
+    if age < 3: print('Your ticket is free')
+    if age < 12: print('Your ticket cost is 10$')
+    if age > 135:
+        print('Peaple do not life so long')
+        active = False
+    else: print('Your ticket cost is 15$')
