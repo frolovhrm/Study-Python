a93a1650a624f6a7bfafa28f0a2ad997801e640a
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-05-12 23:40:38 +0300

    update

---
+++ Unit 8/exercise8_9_11.py
@@ -0,0 +1,34 @@
+"""Упражнение 9"""
+
+# messages = ['Hello', 'How are you', 'When you come']
+#
+# def show_messages(messages):
+#     while messages:
+#         mess = messages.pop()
+#         print(mess)
+#
+#
+# show_messages(messages)
+
+"""Упражнение 10"""
+
+messages = ['Hello', 'How are you', 'When you come']
+sent_messages = []
+num = 0
+messages_2 = messages[:]
+
+def send_message(messages):
+    count = 0
+    while messages:
+        count +=1
+        mess = messages.pop()
+        print(f'{count}. - {mess}\t')
+        sent_messages.append(mess)
+
+send_message(messages_2)
+
+print(f'\nСписок отправленных {sent_messages}')
+
+print(f'\nCписок копия {messages_2}')
+
+print(f'\nИсходный список {messages}')
