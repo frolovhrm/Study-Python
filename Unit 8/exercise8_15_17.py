0ab0015dca431efb4f05651d73fcbfbec3a0087c
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-05-14 00:12:09 +0300

    unit 8 completed

---
+++ Unit 8/exercise8_15_17.py
@@ -0,0 +1,12 @@
+# from func_print_ext import *
+# import func_print_ext
+# from func_print_ext import ext_print as ep
+# import func_print_ext as fpe
+from func_print_ext import ext_print
+
+name = input("What is my mame: ")
+age = int(input("How old am I: "))
+
+
+# fpe.ext_print(age, name)
+ext_print(age,name)
---
+++ Unit 8/func_print_ext.py
@@ -0,0 +1,2 @@
+def ext_print(age, name):
+    print(f"My name is {name}, I'm {age} years old")
