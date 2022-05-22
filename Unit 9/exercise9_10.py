63690ee7b97a594d740933939851ad3929f7a768
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-05-22 23:40:51 +0300

    no comment

--- Unit 9/exercise9_10.py
+++ Unit 9/exercise9_10.py
@@ -1,16 +1,23 @@
-import Restaurant
+from rest import Restaurant
 
 
 class IceCreamStand(Restaurant):
 
-    def __init__(self, restaurant_name, cuisine_type, flavors):
+    def __init__(self, restaurant_name, cuisine_type, flavors=''):
         super().__init__(restaurant_name, cuisine_type)
         self.flavors = flavors
 
     def print_rest(self):
-        print(f'name: {one.restaurant_name}\ncuisine: {one.cuisine_type}\nflavors: {one.flavors}')
+        print(f'name: {self.restaurant_name}\ncuisine: {self.cuisine_type}\nflavors: {self.flavors}')
 
 
 one = IceCreamStand('Funtik', 'IceCream', ['vanilla', 'cream'])
 
+two = Restaurant('Name', 'Type')
+
+one.print_rest()
+two.print_rest()
+print()
 IceCreamStand.print_rest(one)
+Restaurant.print_rest(two)
+
--- Unit 9/rest.py
+++ Unit 9/rest.py
@@ -1,5 +1,10 @@
-class Restaurant():
+class Restaurant:
 
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+
+    def print_rest(self):
+        print(f'name: {self.restaurant_name}\ncuisine: {self.cuisine_type}')
+
+
