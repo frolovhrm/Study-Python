301e60eac1398b3d78e12755ed9fbfdd88011bea
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-06-14 01:22:43 +0300

    not complit

---
+++ Unit 11(tests)/exercise11_3.py
@@ -0,0 +1,18 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, full_salary):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.full_salary = full_salary
+
+    def give_raise(self, new_raise=0):
+
+        if new_raise:
+            return self.full_salary + int(new_raise)
+        else:
+            return self.full_salary + 5000
+
+
+first = Employee('Ivan', 'Ivanov', 5000)
+
+print(first.give_raise(15000))
