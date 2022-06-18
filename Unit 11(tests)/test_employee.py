de2fd21496d443f8e3086b634cfd524da28315cf
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-06-18 23:39:40 +0300

    complit

--- Unit 11(tests)/test_employee.py
+++ Unit 11(tests)/test_employee.py
@@ -5,11 +5,16 @@ from exercise11_3 import Employee
 class TestEmployee(unittest.TestCase):
 
     def setUp(self):
-        self.first_test = Employee('Ivan', 'Ivanov', 5000)
+        self.pers_test = Employee('Ivan', 'Ivanov', 5000)
         self.arg = 5001
 
     def test_give_default_raise(self):
-        self.assertEqual(self.first_test.full_salary + 5000, self.first_test.give_raise())
+        self.assertEqual(self.pers_test.full_salary + 5000, self.pers_test.give_raise())
+
+
+    def test_give_default_raise_arg(self):
+        self.assertEqual(self.pers_test.full_salary + self.arg, self.pers_test.give_raise(self.arg))
+
 
 
 
