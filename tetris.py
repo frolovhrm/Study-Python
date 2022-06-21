77cccf0bde51606b7c363a8c07e86fb2b3e9adcf
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-06-21 11:57:37 +0300

    ***

--- tetris.py
+++ tetris.py
@@ -18,15 +18,23 @@ class Tetris:
         self.cube = Cube(self)
 
     def run_game(self):
-
+        """Запуск основного цикла игры"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.cube.blitme()
+            self._check_events()
+            self._update_screen()
 
-            pygame.display.flip()
+    def _check_events(self):
+        """Обработка нажатия клавиш"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновление изображения на экране"""
+        self.screen.fill(self.settings.bg_color)
+        self.cube.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
