a4804cc33ef32ebbc0ddea1c98ec9694b96f3a27
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-06-21 11:57:50 +0300

    ***

--- cube.py
+++ cube.py
@@ -8,10 +8,10 @@ class Cube():
         self.screen_rect = te_game.screen.get_rect()
 
         """Загружает изображение и создает прямоугольник"""
-        self.image = pygame.image.load('cube.bmp')
+        self.image = pygame.image.load('cube_four.bmp')
         self.rect = self.image.get_rect()
         """Кождый новый корабль появляется у нижнего края экрана"""
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midtop = self.screen_rect.midtop
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ class Settings:
         """Инициализирует настройки игры"""
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (240, 240, 240)
+        self.bg_color = (230, 230, 230)
